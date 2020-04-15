ab197f92fbdb00d553a36712307907a2fa38089f
vadym.sula <sulawados312000@gmail.com>
2020-04-15 13:23:21 +0300

    #83 add: DatabaseConfig: add jdbcTemplate Bean

--- src/main/java/org/deanoffice2/mafiahelper/config/DatabaseConfig.java
+++ src/main/java/org/deanoffice2/mafiahelper/config/DatabaseConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import javax.sql.DataSource;
@@ -28,7 +29,12 @@ public class DatabaseConfig {
     }
 
     @Bean
-    public NamedParameterJdbcTemplate jdbcTemplate(DataSource dataSource) {
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 }
