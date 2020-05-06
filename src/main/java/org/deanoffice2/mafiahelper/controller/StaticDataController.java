1ad59ea8ab13f6a3870111aca3496498f4b255d7
vadym.sula <sulawados312000@gmail.com>
2020-05-06 13:41:51 +0300

    #90 refactor: controller: refactored in ResponseEntity

--- src/main/java/org/deanoffice2/mafiahelper/controller/StaticDataController.java
+++ src/main/java/org/deanoffice2/mafiahelper/controller/StaticDataController.java
@@ -1,5 +1,7 @@
 package org.deanoffice2.mafiahelper.controller;
 
+import org.deanoffice2.mafiahelper.entity.Player;
+import org.deanoffice2.mafiahelper.entity.RoleGame;
 import org.deanoffice2.mafiahelper.service.StaticService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,28 +12,31 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import java.util.List;
 
 @RestController
 public class StaticDataController {
 
     @Autowired
-    @Qualifier(value="staticService")
+    @Qualifier(value = "staticService")
     private StaticService staticService;
 
     @GetMapping("/create-game/")
-    public ResponseEntity getRolesForGame() {
-        return ResponseEntity.ok(staticService.getRoles());
+    public ResponseEntity<List<RoleGame>> getRolesForGame() {
+        return new ResponseEntity<>(
+                staticService.getRoles(),
+                HttpStatus.OK
+        );
     }
 
     @PutMapping("/create-game/")
-    public ResponseEntity addNewPlayer(@RequestBody String playerNickName) {
+    public ResponseEntity<HttpStatus> addNewPlayer(@RequestBody String playerNickName) {
         staticService.addNewPlayer(playerNickName);
         return ResponseEntity.ok(HttpStatus.CREATED);
     }
 
     @GetMapping("/create-game/select-player")
-    public ResponseEntity<Map<Integer, String>> getPlayersNicknames() {
+    public ResponseEntity<List<Player>> getPlayersNicknames() {
         return new ResponseEntity<>(
                 staticService.getPlayersNicknames(),
                 HttpStatus.OK
