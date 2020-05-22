8a31fbf5a6a0074f83ea479d9800703c12e51469
vadym.sula <sulawados312000@gmail.com>
2020-05-22 16:35:44 +0300

    changed data configs for deploying

--- src/main/java/org/deanoffice2/mafiahelper/config/DatabaseConfig.java
+++ src/main/java/org/deanoffice2/mafiahelper/config/DatabaseConfig.java
@@ -16,15 +16,13 @@ import javax.sql.DataSource;
 @PropertySource("classpath:database.properties")
 public class DatabaseConfig {
 
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
     @Bean
-    public DataSource dataSource(@Value("${database.url}") String url,
-                                 @Value("${database.username}") String username,
-                                 @Value("${database.password}") String password) {
-        final HikariConfig config = new HikariConfig();
-        config.setDriverClassName("org.postgresql.Driver");
-        config.setJdbcUrl(url);
-        config.setUsername(username);
-        config.setPassword(password);
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
         return new HikariDataSource(config);
     }
 
