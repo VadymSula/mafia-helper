ac2e26e4e67dcec96ab840a14d2213d14ad3a432
vadym.sula <sulawados312000@gmail.com>
2020-05-26 14:02:07 +0300

    #depoy4 add: controller: add crossOrigin for controllers

--- src/main/java/org/deanoffice2/mafiahelper/controller/StaticDataController.java
+++ src/main/java/org/deanoffice2/mafiahelper/controller/StaticDataController.java
@@ -7,13 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class StaticDataController {
 
