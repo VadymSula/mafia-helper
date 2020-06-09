f27dc885177aae9518dce775929ace7df5a97306
vadym.sula <sulawados312000@gmail.com>
2020-06-09 22:20:02 +0300

    #114 fix: entity: added missed fields

--- src/main/java/org/deanoffice2/mafiahelper/entity/RatingGame.java
+++ src/main/java/org/deanoffice2/mafiahelper/entity/RatingGame.java
@@ -5,15 +5,22 @@ public class RatingGame {
     private Club club;
     private Player player;
     private Float ratingValue;
+    private Integer gamesQuantity;
 
     public RatingGame() {
     }
 
-    public RatingGame(Integer idRating, Club club, Player player, Float ratingValue) {
+    public RatingGame(
+            Integer idRating,
+            Club club,
+            Player player,
+            Float ratingValue,
+            Integer gamesQuantity) {
         this.idRating = idRating;
         this.club = club;
         this.player = player;
         this.ratingValue = ratingValue;
+        this.gamesQuantity = gamesQuantity;
     }
 
     public Integer getIdRating() {
@@ -46,5 +53,13 @@ public class RatingGame {
 
     public void setRatingValue(Float ratingValue) {
         this.ratingValue = ratingValue;
+    }
+
+    public Integer getGames_quantity() {
+        return gamesQuantity;
+    }
+
+    public void setGames_quantity(Integer games_quantity) {
+        this.gamesQuantity = games_quantity;
     }
 }
